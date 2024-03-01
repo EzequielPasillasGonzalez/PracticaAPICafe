9792e9c9acd49135e044101ffc3ad8ec4d031723
Ezequiel <ingepasillasg@gmail.com>
2024-03-01 16:24:01 -0600

    Se configura para cobros con paypal

--- controllers/paypalPayment.controller.js
+++ controllers/paypalPayment.controller.js
@@ -1,54 +1,62 @@
 const {response} = require('express');
 const axios = require('axios');
 
+const createToken = async () => {
+    const params = new URLSearchParams()
+    params.append('grant_type', 'client_credentials')
+
+    const {data} = await axios.post(`${process.env.PAYPAL_URL}/v1/oauth2/token`, params, {
+        auth: {
+            username: process.env.PAYPAL_CLIENT_ID,
+            password: process.env.PAYPAL_SECRET_KEY
+        }
+    })        
+
+    return data.access_token
+}
+
 const createOrder = async (req, res = response) => {
     try {
 
-        const order = {
-            intent: "CAPTURE",
-            purchase_units: [
-              {
-                amount: {
-                  currency_code: "USD",
-                  value: "105.70",
-                },
-              },
-            ],
-            application_context: {
-              brand_name: "mycompany.com",
-              landing_page: "NO_PREFERENCE",
-              user_action: "PAY_NOW",
-              return_url: `capture-order`,
-              cancel_url: `cancel-payment`,
-            },
-        } 
+        const orderList = req.body
 
-        const params = new URLSearchParams()
-        params.append('grant_type', 'client_credentials')
-
-        const {data: {access_token}} =  await axios.post(`${process.env.PAYPAL_URL}/v1/oauth2/token`, params, {
-            auth: {
-                username: process.env.PAYPAL_CLIENT_ID,
-                password: process.env.PAYPAL_SECRET_KEY
-            }
-        })                           
-
-        const config = {
-            headers: {
-                'Authorization': `Bearer ${access_token}` // Reemplaza 'tuToken' con el token real
-            }
-        };
-
-
-        const resp = await axios.post(`${process.env.PAYPAL_URL}/v2/checkout/orders`, order, config)        
-
+        let totalPagar = 0
         
+        orderList.forEach(element => {
+                totalPagar += element.precio                
+        });        
+
+
+        const order = {
+
+            intent: 'CAPTURE',
+            purchase_units: [
+                {
+                    amount: {
+                        currency_code: 'MXN',
+                        value: totalPagar
+                    }
+                }
+            ],            
+        }
+
+        const access_token = await createToken()   
+
+        const resp = await axios.post(`${process.env.PAYPAL_URL}/v2/checkout/orders`, order, {
+            headers: {
+                Authorization: `Bearer ${access_token}`
+            }
+        })            
+    
+
+        console.log(access_token);
 
         return res.json({
             ok: true,
-            body: `${resp}`
+            body: resp.data
         })
-    } catch(error) {
+    } catch(error) {        
+        console.log(error);
         return res.status(400).json({
             ok: false,
             body: `No fue posible realizar el pago ${error}`
@@ -56,6 +64,77 @@ const createOrder = async (req, res = response) => {
 }
 }
 
+const getOrder = async ( req, res = response) => {
+
+    try {
+        const {id} = req.params
+
+        const access_token = await createToken()        
+
+        const resp = await axios.get(`${process.env.PAYPAL_URL}/v2/checkout/orders/${id}`, {
+            headers: {
+                Authorization: `Bearer ${access_token}`
+            }
+        })
+
+        console.log(access_token);
+
+        console.log(resp);
+
+        return res.json({
+            ok: true,
+            body: resp.data
+        })
+    } catch(error) {
+        console.log(error);
+        return res.status(400).json({
+            ok: false,
+            body: `No fue posible buscar el pedido ${error}`
+        })
+    }
+
+}
+
+const captureOrder = async ( req, res = response) => {
+
+    try {
+        const {id} = req.params
+
+        const access_token = await createToken()       
+        
+        console.log(access_token);
+
+        const resp = await axios.post(`${process.env.PAYPAL_URL}/v2/checkout/orders/${id}/capture`, {},{
+            headers: {
+                Authorization: `Bearer ${access_token}`
+            }
+        })
+
+        console.log(resp.data);
+
+        return res.json({
+            ok: true,
+            body: resp.data
+        })
+    } catch(error) {
+        console.log(error.response.data);
+        return res.status(400).json({
+            ok: false,
+            body: `No fue posible buscar el pedido ${error}`
+        })
+    }
+
+}
+
+
+const notifyOrder = ( req, res) => {
+    console.log(req.body);
+    res.send(req.body)
+}
+
 module.exports = {
-    createOrder
+    createOrder,
+    captureOrder,
+    getOrder,
+    notifyOrder
 }
--- routes/paypal.routes.js
+++ routes/paypal.routes.js
@@ -2,12 +2,24 @@ const { Router } = require('express')
 const { check } = require('express-validator')
 
 const { validarCampos, validarJWT } = require('../middlewares/index.middlewares')
-const { createOrder } = require('../controllers/paypalPayment.controller')
+const { createOrder, captureOrder, notifyOrder, getOrder } = require('../controllers/paypalPayment.controller')
 
 const router = Router()
 
 router.post('/create', [
     
 ], createOrder)
+
+router.post('/captureOrder/:id', [
+    
+], captureOrder)
+
+router.get('/getOrder/:id', [
+    
+], getOrder)
+
+router.post('/notifyOrder', [
+    
+], notifyOrder)
 
 module.exports = router
